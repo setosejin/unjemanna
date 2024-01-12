6d3f5b541434f1a9991efab6d699cfcdb385226e
Sejin Kim <lemon05090@gmail.com>
2024-01-12 22:17:45 +0900

    Updates

---
+++ public/_nuxt/index.Jhi2Cnlu.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.xm2CsScf.js";const n={};function r(c,t){return" main page "}const a=e(n,[["render",r]]);export{a as default};
---
+++ server/chunks/app/client.manifest.mjs
@@ -0,0 +1,80 @@
+const client_manifest = {
+  "_vue.f36acd1f.NdWUPawY.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "vue.f36acd1f.NdWUPawY.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.9.1_vite@5.0.11/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-404.QA6BrB_X.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.9.1_vite@5.0.11/node_modules/nuxt/dist/app/entry.js",
+      "_vue.f36acd1f.NdWUPawY.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.SaR2Zalm.css": {
+    "file": "error-404.SaR2Zalm.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [],
+    "file": "error-500.X1AvA0jT.js",
+    "imports": [
+      "_vue.f36acd1f.NdWUPawY.js",
+      "node_modules/.pnpm/nuxt@3.9.1_vite@5.0.11/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.2itWk7wX.css": {
+    "file": "error-500.2itWk7wX.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt@3.9.1_vite@5.0.11/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "dynamicImports": [
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.xm2CsScf.js",
+    "isEntry": true,
+    "src": "node_modules/.pnpm/nuxt@3.9.1_vite@5.0.11/node_modules/nuxt/dist/app/entry.js"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "index.Jhi2Cnlu.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.9.1_vite@5.0.11/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "pages/index.vue"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
